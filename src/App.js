205419531ae15374e4f669f79e15b4229c20c513
baxa88 <mamasolibaxromjon@gmail.com>
2022-03-28 10:43:44 +0500

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React,{useState} from 'react';
+import Search from './cloneMain/Search';
+import URL from './cloneMain/api/URL';
+import './App.css';
+import Video from './cloneMain/Video';
+import ListVideos from './cloneMain/ListVideos';
+
+const App=()=>{
+  const [videos,setVideos]=useState([]);
+  const [selectedVideos,setSelectedVideos]=useState({id:{},snippet:{}});
+
+  async function videoSubmit(searchTerm){
+    const {data:{items:videos}}=await URL.get("search",{
+      params:{
+        part:"snippet",
+        maxResults:5,
+        key:"AIzaSyDlvw4MzzjaxJp5v8Q8eq2P4lF5uXTZwrc",
+        q:searchTerm
+      }
+    })
+    console.log(videos)
+    setVideos(videos)
+    setSelectedVideos(videos[0])
+  }
+
+  return(
+  <>
+    <div className='searchSec'>
+      <Search onSubmit={videoSubmit}/>
+      </div>
+      <div className='videoMain'>
+        <div className='videoSec'>
+          <Video video={selectedVideos}/>
+        </div>
+        <div className='videoLists'>
+          <ListVideos videos={videos} onVideoSelect={setSelectedVideos}/>
+        </div>
+      </div>
+  </>
+  )
+}
+export default App;
+
---
+++ src/cloneMain/Search.js
@@ -0,0 +1,42 @@
+import React,{useState} from 'react';
+import logo from './images/logo1.png';
+import functions from './images/functions.png';
+
+
+const Search = ({onSubmit}) => {
+    const [searchTerm,setSearchTerm]=useState("")
+    const handleChange=(event)=>setSearchTerm(event.target.value);
+
+const keyPress=(event)=>{
+    if(event.key==="Enter"){
+        onSubmit(searchTerm)
+    }
+}
+
+    return (
+        <>
+            <div className='header'>
+                <a href="/" className='brand'>
+                    <img src={logo} alt="logo" className='logoBrand'/>
+                </a>
+                <input 
+                type="text"
+                fullWidth
+                placeholder="Search.."
+                value={searchTerm}
+                onChange={handleChange}
+                onKeyPress={keyPress}
+                />
+                <div className='icons'>
+                    <img src={functions} 
+                    alt="functions" 
+                    className='functions'
+                    />
+                   
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Search;
